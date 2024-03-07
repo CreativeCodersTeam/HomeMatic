840d1ffc2a681040b889e154c02a9422b5b20eb6
darthsharp <48331467+darthsharp@users.noreply.github.com>
2024-03-07 19:49:52 +0100

    Refactor Backend program structure
    The original Backend program layout has been significantly changed, including the extraction of the initialization logic into a separate Startup class. This makes our code cleaner and more organized, simplifying the structure of Main by moving services configuration and middleware sequence to the Startup class.

--- source/Tools/ControlCenter/CreativeCoders.HomeMatic.Tools.ControlCenter.Backend/Program.cs
+++ source/Tools/ControlCenter/CreativeCoders.HomeMatic.Tools.ControlCenter.Backend/Program.cs
@@ -1,50 +1,13 @@
-using CreativeCoders.AspNetCore.Jwt;
-using CreativeCoders.AspNetCore.TokenAuth;
-using CreativeCoders.Core;
-using CreativeCoders.Core.Text;
-using CreativeCoders.HomeMatic.Tools.ControlCenter.Backend;
-using CreativeCoders.HomeMatic.Tools.ControlCenter.Backend.Repositories;
+namespace CreativeCoders.HomeMatic.Tools.ControlCenter.Backend;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers()
-    .AddApplicationPart(typeof(TokenAuthController).Assembly);
-
-builder.Services.AddJwtSupport<HomeMaticTokenAuthHandler>(RandomString.Create());
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddCors(x => x.AddDefaultPolicy(x => x.AllowAnyOrigin()));
-
-builder.Services.AddHmccRepositories();
-
-var app = builder.Build();
-
-app.Use(async (context, next) =>
+public static class Program
 {
-    context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
-    context.Response.Headers.Add("Access-Control-Allow-Methods", "*");
-    context.Response.Headers.Add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    await next();
-});
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
 
-app.UseCors();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-
-app.MapControllers();
-
-app.Run();
---
+++ source/Tools/ControlCenter/CreativeCoders.HomeMatic.Tools.ControlCenter.Backend/Startup.cs
@@ -0,0 +1,57 @@
+﻿using CreativeCoders.AspNetCore.Jwt;
+using CreativeCoders.AspNetCore.TokenAuth;
+using CreativeCoders.Core.Text;
+using CreativeCoders.HomeMatic.Tools.ControlCenter.Backend.Repositories;
+
+namespace CreativeCoders.HomeMatic.Tools.ControlCenter.Backend;
+
+public class Startup(IConfiguration configuration)
+{
+    public IConfiguration Configuration { get; } = configuration;
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers()
+            .AddApplicationPart(typeof(TokenAuthController).Assembly);
+
+        services.AddJwtSupport<HomeMaticTokenAuthHandler>(RandomString.Create());
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.AddCors(x => x.AddDefaultPolicy(x => x.AllowAnyOrigin()));
+
+        services.AddHmccRepositories();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.Use(async (context, next) =>
+        {
+            context.Response.Headers.Append("Access-Control-Allow-Origin", "*");
+            context.Response.Headers.Append("Access-Control-Allow-Methods", "*");
+            context.Response.Headers.Append("Access-Control-Allow-Headers",
+                "Origin, X-Requested-With, Content-Type, Accept");
+            await next();
+        });
+
+
+        app.UseCors();
+
+// Configure the HTTP request pipeline.
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+    }
+}
