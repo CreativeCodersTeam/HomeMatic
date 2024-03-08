d0c3a768d3fb5876a755e1d13af49585204852c9
darthsharp <48331467+darthsharp@users.noreply.github.com>
2024-03-08 18:00:45 +0100

    Refactor auth service adding login and logout
    The authentication service has been updated with the addition of actual login and logout functionalities. Previously commented-out login and logout methods have been replaced with versions that make calls to '/api/tokenauth/login' and '/api/tokenauth/logout'. These new methods are complete with error handling and value mapping.

--- source/Tools/ControlCenter/hmcc/src/app/auth/login-page/auth.service.ts
+++ source/Tools/ControlCenter/hmcc/src/app/auth/login-page/auth.service.ts
@@ -1,7 +1,6 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import * as moment from "moment/moment";
-import {shareReplay, tap} from "rxjs";
+import {catchError, map, of, shareReplay} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -12,22 +11,24 @@ export class AuthService {
 
   }
 
-  // login(email:string, password:string ) {
-  //   return this.http.post('/api/login', {email, password})
-  //     .pipe(tap(res => this.setSession), shareReplay(1));
-  // }
-  //
-  // private setSession(authResult) {
-  //   const expiresAt = moment().add(authResult.authToken, 'second');
-  //
-  //   localStorage.setItem('id_token', authResult.authToken);
-  //   localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()) );
-  // }
-  //
-  // logout() {
-  //   localStorage.removeItem("id_token");
-  //   localStorage.removeItem("expires_at");
-  // }
+  login(email: string, password: string) {
+    return this.http
+      .post('/api/tokenauth/login', {userName: email, password: password})
+      .pipe(
+        map(() => true),
+        catchError(() => of(false)),
+        shareReplay(1));
+  }
+
+  logout() {
+    return this.http
+      .post('/api/tokenauth/logout', {})
+      .pipe(
+        map(() => true),
+        catchError(() => of(false)),
+        shareReplay(1));
+  }
+
   //
   // public isLoggedIn() {
   //   return moment().isBefore(this.getExpiration());
@@ -46,9 +47,5 @@ export class AuthService {
   //
   //   const expiresAt = JSON.parse(expiration);
   //   return moment(expiresAt);
-  // }
-
-  // public login(username: string, password: string) {
-  //   return true;
   // }
 }
