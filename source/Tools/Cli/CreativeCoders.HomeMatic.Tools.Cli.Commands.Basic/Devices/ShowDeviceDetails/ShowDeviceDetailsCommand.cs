dfd2399ce0533ae7619719771b8daa0a2c4fdd13
darthsharp <48331467+darthsharp@users.noreply.github.com>
2025-07-18 16:41:54 +0200

    Enhance `ShowDeviceDetailsCommand` to include control and type details for parameter sets

--- source/Tools/Cli/CreativeCoders.HomeMatic.Tools.Cli.Commands.Basic/Devices/ShowDeviceDetails/ShowDeviceDetailsCommand.cs
+++ source/Tools/Cli/CreativeCoders.HomeMatic.Tools.Cli.Commands.Basic/Devices/ShowDeviceDetails/ShowDeviceDetailsCommand.cs
@@ -80,6 +80,10 @@ public class ShowDeviceDetailsCommand : IHomeMaticCliCommandWithOptions<ShowDevi
             {
                 _console.WriteLine(
                     $"{indent}  - {paramSetValue.ParamSetValue.Name} : {paramSetValue.ParamSetValue.Value}");
+                _console.WriteLine(
+                    $"{indent}    Control: {paramSetValue.Description.Control}");
+                _console.WriteLine(
+                    $"{indent}    Type: {paramSetValue.Description.Type}");
             }
         }
     }
