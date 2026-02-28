c5156754d2cab597921f012416ad1d9f0f00e53d
darthsharp <48331467+darthsharp@users.noreply.github.com>
2026-02-28 16:43:56 +0100

    migrate(build): switch from Nuke to Cake for build process
    Replaced Nuke build system with Cake for better flexibility and maintainability. Added `Build.csproj`, `BuildContext.cs`, and supporting files while removing Nuke-related configurations, workflows, and scripts. Updated dependencies and workflows to align with the new setup.

---
+++ build/BuildContext.cs
@@ -0,0 +1,103 @@
+using Cake.Common.Build;
+using Cake.Core;
+using Cake.Core.IO;
+using CreativeCoders.CakeBuild;
+using CreativeCoders.CakeBuild.Tasks.Defaults;
+using CreativeCoders.CakeBuild.Tasks.Templates.Settings;
+using CreativeCoders.Core;
+using CreativeCoders.Core.Collections;
+using CreativeCoders.Core.IO;
+using JetBrains.Annotations;
+
+namespace Build;
+
+[UsedImplicitly]
+public class BuildContext(ICakeContext context)
+    : CakeBuildContext(context), IDefaultTaskSettings, ICreateDistPackagesTaskSettings, ICreateGitHubReleaseTaskSettings
+{
+    public IList<DirectoryPath> DirectoriesToClean => this.CastAs<ICleanTaskSettings>()
+        .GetDefaultDirectoriesToClean().AddRange(RootDir.Combine(".tests"));
+
+
+    public string Copyright => $"{DateTime.Now.Year} CreativeCoders";
+
+    public string PackageProjectUrl => "https://github.com/CreativeCodersTeam/HomeMatic";
+
+    public string PackageLicenseExpression => PackageLicenseExpressions.ApacheLicense20;
+
+    public string NuGetFeedUrl => this.GitHubActions().Environment.Workflow.Workflow == "release"
+        ? "nuget.org"
+        : "https://nuget.pkg.github.com/CreativeCodersTeam/index.json";
+
+    public bool SkipPush => this.BuildSystem().IsPullRequest ||
+                            this.BuildSystem().IsLocalBuild ||
+                            this.GitHubActions().Environment.Runner.OS != "Linux";
+
+    public DirectoryPath PublishOutputDir => ArtifactsDir.Combine("published");
+
+    private const string CliPath = "source/Tools/Cli/CreativeCoders.HomeMatic.Tools.Cli";
+
+    private const string CliProjectFile = "CreativeCoders.HomeMatic.Tools.Cli.csproj";
+
+    public IEnumerable<PublishingItem> PublishingItems =>
+    [
+        new PublishingItem(
+            RootDir
+                .Combine(CliPath)
+                .CombineWithFilePath(CliProjectFile),
+            PublishOutputDir.Combine("cli")),
+        new PublishingItem(
+            RootDir
+                .Combine(CliPath)
+                .CombineWithFilePath(CliProjectFile),
+            PublishOutputDir.Combine("cli-win64"))
+        {
+            Runtime = "win-x64",
+            SelfContained = true
+        },
+        new PublishingItem(
+            RootDir
+                .Combine(CliPath)
+                .CombineWithFilePath(CliProjectFile),
+            PublishOutputDir.Combine("cli-win64-no-selfcontained"))
+        {
+            Runtime = "win-x64",
+            SelfContained = false
+        },
+        new PublishingItem(
+            RootDir
+                .Combine(CliPath)
+                .CombineWithFilePath(CliProjectFile),
+            PublishOutputDir.Combine("cli-win-arm64"))
+        {
+            Runtime = "win-arm64",
+            SelfContained = true
+        }
+    ];
+
+    private const string DistPackageName = "GitTool.Cli";
+
+    public IEnumerable<DistPackage> DistPackages =>
+    [
+        new DistPackage(DistPackageName, PublishOutputDir.Combine("cli"))
+    ];
+
+    public string ReleaseName => $"v{Version.FullSemVer}";
+
+    public string ReleaseVersion => $"v{Version.FullSemVer}";
+
+    public string ReleaseBody => "GitTools Release";
+
+    public bool IsPreRelease => !string.IsNullOrWhiteSpace(Version.PreReleaseTag);
+
+    private DirectoryPath SetupOutputDir => ArtifactsDir.Combine("setups");
+
+    public IEnumerable<GitHubReleaseAsset> ReleaseAssets =>
+    [
+        new GitHubReleaseFileAsset(
+            FileSys.Directory.EnumerateFiles(SetupOutputDir.FullPath, "*.exe").First(), null),
+        new GitHubReleaseFileAsset(
+            GetRequiredSettings<ICreateDistPackagesTaskSettings>().DistOutputPath
+                .CombineWithFilePath(DistPackageName + ".tar.gz").FullPath, null)
+    ];
+}
